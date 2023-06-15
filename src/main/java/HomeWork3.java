9196854c9fd401dd2adf671740bc9617cb60fa1a
roman lepihov <lepihovrom@yandex.by>
2023-06-15 09:33:26 +0300

    [update] hw 3

--- src/main/java/HomeWork3.java
+++ src/main/java/HomeWork3.java
@@ -7,9 +7,6 @@
 // 3) Найти максимальное значение
 // 4) Найти среднее значение
 
-import org.w3c.dom.ls.LSOutput;
-
-import javax.swing.text.MaskFormatter;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Iterator;
@@ -26,7 +23,6 @@ public class HomeWork3 {
         System.out.println(getMin(arrList));
         System.out.println(getMax(arrList));
         System.out.println(getAverage(arrList));
-        System.out.println(getAverage2(arrList));
     }
 
     public static Double getAverage(ArrayList<Integer> list) {
@@ -38,28 +34,6 @@ public class HomeWork3 {
         return list.size() > 0 ? (double) sum / list.size() : 0;
     }
 
-    public static Integer getAverage2(ArrayList<Integer> list) {
-        //средний по значению элемент
-        if (list.size() < 3) {
-            return 0;
-        }
-
-        Collections.sort(list);
-        System.out.println(list);
-
-        if (list.size() % 2 != 0) {
-            return list.get(list.size() / 2);
-        } else {
-            int index = list.size() / 2;
-            double middle = (double) (Collections.max(list) + Collections.min(list)) / 2;
-            if (middle - list.get(index - 1) < list.get(index) - middle) {
-                return list.get(index -1 );
-            } else {
-                return list.get(index);
-            }
-        }
-    }
-
     public static Integer getMin(ArrayList<Integer> list) {
         return Collections.min(list);
     }
@@ -69,7 +43,8 @@ public class HomeWork3 {
     }
 
     public static void delEven(ArrayList<Integer> list) {
-//        for (Iterator<Integer> it = list.iterator(); it.hasNext();) {
+//        Iterator<Integer> it = list.iterator();
+//        while (it.hasNext()){
 //            if (it.next() % 2 == 0) {
 //                it.remove();
 //            }
@@ -86,6 +61,4 @@ public class HomeWork3 {
 
         return list;
     }
-
-
 }
